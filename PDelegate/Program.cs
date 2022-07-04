e6925411ea7ea3537dd2fa54b32caefa09aac53b
INA <codechemi@gmail.com>
2022-07-04 09:50:44 +0900

    포스트 별 프로젝트 구분.

---
+++ PDelegate/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace PDelegate
+{
+    class Program
+    {
+        delegate void TestDelegate(int Number);
+
+        static void main(string[] args)
+        {
+            #region Lamda
+            // Lamda
+            Action<int, int>? RamdaA = (int x, int y) => Add(x, y);
+            Action<int, int>? RamdaB = (x, y) => { Console.WriteLine(x + y); };
+            Func<int, int, int>? RamdaC = (x, y) => { return x + y; };
+
+            RamdaA(1234, 5678);
+            RamdaB(1234, 5678);
+
+            // 무명 메서드
+            var RamdaD = delegate (int x, int y)
+            {
+                Console.WriteLine(x + y);
+            };
+
+            RamdaD(1234, 5678);
+
+            #endregion
+
+            #region Action,Func
+            // Action<T>,Func<T>,<TResult>
+            Action<int> test = new Action<int>(WriteConsole);
+            test(1234);
+            // 1234
+            #endregion
+
+            #region Delegate Chain
+            // 델리게이트 체인 적용.
+            var Chain = new Action<int, int>(Add);
+            Chain += new Action<int, int>(Minus);
+            Chain += new Action<int, int>(Division);
+            Chain += new Action<int, int>(Multiply);
+
+            Chain(1234, 5678);
+
+            // 델리게이트 체인 미적용.
+            Add(1234, 5678);
+            Minus(1234, 5678);
+            Division(1234, 5678);
+            Multiply(1234, 5678);
+            #endregion
+        }
+
+        static void Add(int x, int y)
+        {
+            Console.WriteLine(x + y);
+        }
+
+        static void Minus(int x, int y)
+        {
+            Console.WriteLine(x - y);
+        }
+
+        static void Division(int x, int y)
+        {
+            Console.WriteLine(x / y);
+        }
+
+        static void Multiply(int x, int y)
+        {
+            Console.WriteLine(x * y);
+        }
+
+        static void WriteConsole(int Number)
+        {
+            Console.WriteLine(Number);
+        }
+    }
+}
---
+++ PGenericCollections/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace GenericCollections
+{
+    public class Program
+    {
+        static void Main(string[] arg)
+        {
+            AddListArray();
+            AddDictionary();
+        }
+
+        static void AddListArray()
+        {
+            List<string> StringList = new List<string>();
+            // 선언 시 배열 길이 필수 지정
+            string[] StringArray = new string[5];
+
+            for (int i = 0; i < StringArray.Length; i++)
+            {
+                // 값 대입 시 인덱스 지정 필수
+                StringArray[i] = i.ToString();
+                StringList.Add(i.ToString());
+            }
+
+            StringArray[1] = "Insert";
+
+            StringList.Insert(1, "Insert");
+            StringList.Remove("Insert");
+        }
+
+        static void AddDictionary()
+        {
+            // Dictionary<KeyType, ValueType> dic = new Dictionary<KeyType, ValueType>();
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            dic.Add("Key A", "Value A");
+            // 중복된 키 삽입 시 에러 발생.
+            dic.Add("Key B", "Value B");
+
+            Console.WriteLine(dic["Key A"]);
+        }
+    }
+}
