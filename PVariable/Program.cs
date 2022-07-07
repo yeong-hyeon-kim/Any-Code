d0b2da3fed63da1098a5b5faa1ffc34241a6749f
INA <codechemi@gmail.com>
2022-07-07 11:41:09 +0900

    변수, 상수 코드 추가.

---
+++ PVariable/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Variable
+{
+    class Program
+    {
+        const int FixValue = 1234;
+        readonly int VarValue = 5678;
+
+        public Program()
+        {
+            Console.WriteLine("초기값 : " + VarValue);
+            VarValue = 1234;
+        }
+
+        static void Main(string[] args)
+        {
+            //FixValue = 5678;
+
+            Program program = new Program();
+            Console.WriteLine("할당값 : " + program.VarValue);
+        }
+    }
+}
