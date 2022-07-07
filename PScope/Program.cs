b56f1527aaa7c17a27226f8b7d8be9e522990130
INA <codechemi@gmail.com>
2022-07-07 14:35:34 +0900

    유효범위 코드 추가.

---
+++ PScope/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace PScope
+{
+    delegate void ScopeCallback(object sender, EventArgs e);
+
+    class Scope
+    {
+        void Method()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+
+            }
+        }
+    }
+}
