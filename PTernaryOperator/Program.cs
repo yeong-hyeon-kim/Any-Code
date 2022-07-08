6af3bff7803d4abbbe81d550c805c28778b5bec2
INA <codechemi@gmail.com>
2022-07-08 16:39:44 +0900

    삼항 연산자 코드 추가.

---
+++ PTernaryOperator/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace PTernaryOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Run();
+        }
+
+        static void Run()
+        {
+            int x = 1;
+            Console.WriteLine(x == 1 ? "참(True)" : "거짓(False)");
+
+            if (x == 1)
+            {
+                Console.WriteLine("참(True)");
+            }
+            else
+            {
+                Console.WriteLine("거짓(False)");
+            }
+        }
+    }
+}
