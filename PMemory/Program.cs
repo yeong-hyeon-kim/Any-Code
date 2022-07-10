f1c743df7e7e92f54d02d9275bb9cecbd104736c
INA <codechemi@gmail.com>
2022-07-11 02:18:30 +0900

    메모리 구조 코드 추가.

---
+++ PMemory/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace PMemory
+{
+    class Program
+    {
+        static string FieldText = "";
+
+        void Main()
+        {
+            Action action = () =>
+            {
+                Console.WriteLine("Memory");
+            };
+        }
+
+        static void Add(int x, int y)
+        {
+            Console.WriteLine(x + y);
+        }
+
+        static void Minus(int x, int y)
+        {
+            Console.WriteLine(x - y);
+        }
+
+        static void Division(int x, int y)
+        {
+            Console.WriteLine(x / y);
+        }
+
+        static void Multiply(int x, int y)
+        {
+            Console.WriteLine(x * y);
+        }
+
+        static void WriteConsole(int Number)
+        {
+            string PreFix = "Value : ";
+
+            try
+            {
+                Console.WriteLine(PreFix + Number);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+    }
+}
