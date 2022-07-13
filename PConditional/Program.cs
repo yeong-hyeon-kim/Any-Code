51151cb03690aa3e1dd494a3e24346d3063cb8cd
INA <codechemi@gmail.com>
2022-07-13 17:29:26 +0900

    조건문(Conditional Statements) 코드 추가.

---
+++ PConditional/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace PConditionalStatements
+{
+    
+    class Program
+    {
+        static void Main()
+        {
+            IfConditional();
+            SwitchConditional();
+        }
+
+        static void IfConditional()
+        {
+            bool ConditionalA = true;
+            bool ConditionalB = true;
+
+            // if
+            if (ConditionalA)
+                Console.WriteLine("TRUE!");
+
+            // if else
+            if (ConditionalA)
+            {
+                Console.WriteLine("TRUE!");
+            }
+            else
+            {
+                Console.WriteLine("FALSE!");
+            }
+
+            // if else if
+            if (ConditionalA)
+            {
+                Console.WriteLine("TRUE!");
+            }
+            else if (ConditionalB)
+            {
+                Console.WriteLine("ELSE TRUE!");
+            }
+            else
+            {
+                Console.WriteLine("ALL FALSE!");
+            }
+        }
+
+        static void SwitchConditional()
+        {
+            string Animal = "북극곰";
+            
+            switch (Animal)
+            {
+                case "북극곰":
+                    Console.WriteLine(Animal);
+                    break;
+                case "불곰":
+                    Console.WriteLine(Animal);
+                    break;
+                case "반달곰":
+                    Console.WriteLine(Animal);
+                    break;
+                default:
+                    Console.WriteLine(Animal);
+                    break;
+            }
+        }
+    }
+}
