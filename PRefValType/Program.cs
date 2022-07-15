61d15c3636a40f3078ead3668cbe8e69138ea229
INA <codechemi@gmail.com>
2022-07-15 11:16:24 +0900

    RefValType 코드 추가.

--- PRefValType/Program.cs
+++ PRefValType/Program.cs
@@ -9,6 +9,9 @@
         static void Main()
         {
             ValueType();
+
+            int x = 9999;
+            ReferenceType(ref x);
         }
 
         /// <summary>
@@ -23,6 +26,15 @@
 
             // Reference Type : 주소를 참조하기 때문에 실질적인 값은 힙(Heap)에 남아있다.
             Reference reference = new Reference();
+        }
+
+        /// <summary>
+        /// 매개 변수는 값 타입이라 독립적이지만 ref 키워드로 인해 인자의 주소 값을 참조하는 참조 형식이 되어 인자의 값이 변경된다.
+        /// </summary>
+        /// <param name="x"></param>
+        static void ReferenceType(ref int x)
+        {
+            x = 1234;
         }
     }
 }
