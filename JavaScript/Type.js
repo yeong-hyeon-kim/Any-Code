cf0177849e10920c7ebdb67c86ae693e296f1734
INA <codechemi@gmail.com>
2022-08-11 15:06:23 +0900

    자바스크립트(JavaScript) 타입 코드 추가.

---
+++ JavaScript/Type.js
@@ -0,0 +1,23 @@
+// 기본형 : 값 타입(Value Type).
+let Number = 100;
+let String = "String";
+let Symbol = new Symbol();
+let Boolean = true;
+// 사용자가 명시적으로 `없음`을 표현.
+let NUll = null;
+// 사용자가 값을 지정할 것이라고 예상했지만 지정하지 않았을 때 반환.
+let UnDefined = undefined;
+
+// 참조형 : 참조 타입(Reference Type).
+let Object = {
+  Key: "",
+  Value: "",
+};
+
+let Array = [1, 2, 3, 4];
+
+let Function = function () {
+  console.log("Function!");
+};
+
+let Date = new Date().getDate();
