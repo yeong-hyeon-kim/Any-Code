cb7b0449dcf5a14e20f3ae4cfc7b73407fc7a734
INA <codechemi@gmail.com>
2022-09-28 14:27:38 +0900

    리액트(React) 다이어그램 추가.

---
+++ JavaScript/React-Introduce.js
@@ -0,0 +1,9 @@
+const checkIn = document.getElementById("CheckIn");
+const checkOut = document.getElementById("CheckOut");
+
+checkIn.onclick = () => {
+  alert("Check In!");
+};
+checkOut.onclick = () => {
+  alert("Check Out!");
+};
