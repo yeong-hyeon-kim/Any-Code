68a87efda2e7fceb3578ca2e4482d397369de070
INA <codechemi@gmail.com>
2023-02-08 11:00:23 +0900

    자바스크립트(JavaScript) - 로컬 스토리지(LocalStorage), 세션 스토리지(Session Storage)

---
+++ JavaScript/src/LocalStorage.js
@@ -0,0 +1,8 @@
+// 로컬 스토리지에 데이터 저장
+localStorage.setItem("id", "user");
+// 로컬 스토리지에서 데이터 읽기
+localStorage.getItem("id");
+// 로컬 스토리지에서 데이터 삭제
+localStorage.removeItem("id");
+// 로컬 스토리지 데이터 초기화
+localStorage.clear();
---
+++ JavaScript/src/SessionStorage.js
@@ -0,0 +1,8 @@
+// 세션 스토리지에 데이터 저장
+sessionStorage.setItem("id", "user");
+// 세션 스토리지에서 데이터 읽기
+sessionStorage.getItem("id");
+// 세션 스토리지에서 데이터 삭제
+sessionStorage.removeItem("id");
+// 세션 스토리지 데이터 초기화
+sessionStorage.clear();
